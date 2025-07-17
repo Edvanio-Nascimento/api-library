626a4b1f7254e08c3907ecb14bb6c557de4b9571
Edy Nascimento <nascoliedy@gmail.com>
2025-07-17 09:49:13 -0400

    Starting project

---
+++ src/test/java/com/edy/course/library/ApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edy.course.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
